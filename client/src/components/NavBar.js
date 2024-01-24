2fb858b4c2fb03dcabe0736b47257f821ffd5569
Kimberly Palmer <kimberlylpalmer@gmail.com>
2024-01-24 07:09:28 -0800

    added styling to css

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,31 +1,33 @@
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 
+
 /* define the NavBar component */
 function NavBar() {
   return (
     <nav>
+      {/* <div class="logo"></div> */}
       <NavLink
         to="/"
         /* add styling to Navlink */
-        className="nav-link"
-      >
+        className="nav-link homeLink"
+        activeClassName="active">
         🏠
       </NavLink>
       <NavLink
         to="/login"
-        className="nav-link"
-      >
+        className="nav-link loginLink"
+        activeClassName="active">
         🧑🔓
       </NavLink>
       <NavLink
         to="/signup"
-        className="nav-link"
-      >
+        className="nav-link signupLink"
+        activeClassName="active">
         🧑✍️
       </NavLink>
     </nav>
   );
-};
+}
 
 export default NavBar;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,3 +1,4 @@
+// import logo from './assets/PathfindersParadiseLogo.png';
 import "../styles.css";
 import React from "react";
 import NavBar from "../components/NavBar";
@@ -8,10 +9,21 @@ function Home() {
       <header>
         <NavBar />
       </header>
-      <main>
-        <h1 className="home-header">PATHFINDERS</h1>
-        <h1 className="home-header">PARADISE</h1>
-      </main>
+      <div className="gridContainer">
+        <div className="leftColumn column">
+          <p> Embark on a world of adventure and let Pathfinders Paradise be your trusty companion.</p>
+        </div>
+        <main className="homeBackground">
+          <div>
+            <h1 className="home-header">PATHFINDERS</h1>
+            <h1 className="home-header">PARADISE</h1>
+          <div className="mainContent"></div>
+          </div>
+        </main>
+        <div className="rightColumn column">
+          <p>Start your adventure today – your future self will thank you for the memories!</p>
+        </div>
+      </div>
     </>
   );
 }
