b2d7cb5195fae5cf857ff19649ad4642205fcec7
Hanna Negash <hnegash.n@gmail.com>
2024-01-24 17:09:28 -0800

    Add Get, Delete, and Patch requests for trips

--- client/src/pages/Trips.js
+++ client/src/pages/Trips.js
@@ -1,29 +1,95 @@
 import NavBar from "../components/NavBar";
+import React, { useEffect, useState } from "react";
+import TripDetails from "./TripDetails";
+import CreateTripForm from "./CreateTripForm";
 
 function Trips() {
-    return (
-      <>
-        <header>
-          <NavBar />
-        </header>
-        <main>
-          <h1>Please Login to see your Trips!</h1>
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("/trips")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then(data => setData(data))
+      .catch(error =>
+        console.log(
+          "There has been a problem with your fetch operation: ",
+          error,
+        ),
+      );
+  }, []);
+
+  function updateTrip(updatedTrip) {
+    fetch(`/trips/${updatedTrip.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedTrip),
+    })
+      .then(res => res.json())
+      .then(updatedTrip => {
+        setData(
+          data.map(trip => (trip.id === updatedTrip.id ? updatedTrip : trip)),
+        );
+      })
+      .catch(error => console.log("Error:", error));
+  }
+
+  function deleteTrip(id) {
+    fetch(`/trips/${id}`, {
+      method: "DELETE",
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        // Remove the trip from the local state
+        setData(data.filter(trip => trip.id !== id));
+      })
+      .catch(error =>
+        console.log(
+          "There has been a problem with your fetch operation: ",
+          error,
+        ),
+      );
+  }
+
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <h1>Please Login to see your Trips!</h1>
+        <div>
+          <h3>Recent User Trips</h3>
+          <p>
+            <strong>Marie578:</strong> Dallas, USA <br></br>
+            <em>Dates:</em> October 2024 <br></br>
+            <em>Occasion:</em> Wedding
+          </p>
           <div>
-            <h3>Recent User Trips</h3>
-            <p>
-              <strong>Marie578:</strong> Dallas, USA <br></br>
-              <em>Dates:</em>  October 2024  <br></br>
-              <em>Occasion:</em> Wedding
-            </p>
-            {/* <p>
-            {user.name}: {destination.city}, {destination.country}  
-            Dates: {trip.start_date.month + trip.start_date.year}  
-            Occasion: {trip.occasion}
-            </p> */}
+            <CreateTripForm trips={data} />
+            {data.map(trip => (
+              <TripDetails
+                key={trip.id}
+                trip={trip}
+                deleteTrip={deleteTrip}
+                updateTrip={updateTrip}
+                data={data}
+                setData={setData}
+              />
+            ))}
+          </div>
         </div>
-        </main>
-      </>
-    );
-  };
-  
-  export default Trips;
+      </main>
+    </>
+  );
+}
+
+export default Trips;
